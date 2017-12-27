8a42ee3eeb85e332442a3e1ed4c6534bf9dfba8d
HP <kunalchowda@gmail.com>
2017-12-27 16:14:34 +0530

    Initial commit

---
+++ app/src/main/java/info/androidhive/youtubeplayer/Config.java
@@ -0,0 +1,10 @@
+package info.androidhive.youtubeplayer;
+
+public class Config {
+	// Google Console APIs developer key
+	// Replace this key with your's
+	public static final String DEVELOPER_KEY = "AIzaSyATTx4nuX2I7StrAlnuS5hR6n4PKqQ1Wxo";
+	
+	// YouTube video id
+	public static final String YOUTUBE_VIDEO_CODE = "_oEA18Y8gM0";
+}
